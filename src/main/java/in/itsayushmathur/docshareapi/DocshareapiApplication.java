9de312a6477dfc3e1dbe204e50e9eda0e1702168
itsayushmathur <mathurayush121@gmail.com>
2025-08-08 18:11:09 +0530

    Initialization

---
+++ src/main/java/in/itsayushmathur/docshareapi/DocshareapiApplication.java
@@ -0,0 +1,13 @@
+package in.itsayushmathur.docshareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocshareapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocshareapiApplication.class, args);
+	}
+
+}
